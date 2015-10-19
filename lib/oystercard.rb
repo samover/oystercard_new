0e81705108d2970f74d82df3a3c604af976fe7a7
ALRW <awerner1@googlemail.com>
2015-10-19 17:26:30 +0100

    added limit to topups

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,5 +1,6 @@
 class Oystercard
 
+  LIMIT = 90
   attr_reader :balance
 
   def initialize
@@ -7,6 +8,7 @@ class Oystercard
   end
 
   def top_up(money)
+    fail "The topup limit is £#{LIMIT}" if @balance + money > 90
     @balance += money
   end
 
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -5,6 +5,7 @@ describe Oystercard do
     # expect(subject.balance).to be(0)
   end
 
+  
   describe '#top_up' do
 
     it {is_expected.to respond_to(:top_up).with(1).argument}
@@ -12,6 +13,12 @@ describe Oystercard do
     it 'can top up the balance' do
       expect {subject.top_up(1)}.to change{subject.balance}.by 1
     end
+
+    it 'fails if more than £90 is added' do
+      expect{subject.top_up(100)}.to raise_error("The topup limit is £#{Oystercard::LIMIT}")
+    end
+
+
 
     # it 'adds money' do
     #   expect(subject.top_up(15)).to eq subject.balance
