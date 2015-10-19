2fa6483559982c347c7e04ea874a40e315febcb4
“DovileSand” <dovile.sand@protonmail.ch>
2015-10-19 17:07:36 +0100

    Added top_up method

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -4,4 +4,17 @@ describe Oystercard do
     expect(subject.balance).to be_truthy
     # expect(subject.balance).to be(0)
   end
+
+  describe '#top_up' do
+
+    it {is_expected.to respond_to(:top_up).with(1).argument}
+
+    it 'can top up the balance' do
+      expect {subject.top_up(1)}.to change{subject.balance}.by 1
+    end
+
+    # it 'adds money' do
+    #   expect(subject.top_up(15)).to eq subject.balance
+    # end
+  end
 end
