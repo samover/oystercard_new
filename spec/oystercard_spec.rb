042550cefc4d79f303705c0098d7fe1e544dab8b
“DovileSand” <dovile.sand@protonmail.ch>
2015-10-20 11:39:39 +0100

    Introduced deduct method

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -5,7 +5,7 @@ describe Oystercard do
     # expect(subject.balance).to be(0)
   end
 
-  
+
   describe '#top_up' do
 
     it {is_expected.to respond_to(:top_up).with(1).argument}
@@ -18,7 +18,13 @@ describe Oystercard do
       expect{subject.top_up(100)}.to raise_error("The topup limit is £#{Oystercard::LIMIT}")
     end
 
+    describe '#deduct' do
 
+      it 'deducts the amount in the argument from the balance' do
+        expect{subject.deduct(1)}.to change{subject.balance}.by(-1)
+      end
+
+    end
 
     # it 'adds money' do
     #   expect(subject.top_up(15)).to eq subject.balance
